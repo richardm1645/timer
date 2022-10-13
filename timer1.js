87ed5b05ca48c709a28b5d2c4ede1c50fb2e01b6
Richard Miao <richardmiao1645@gmail.com>
2022-10-13 16:16:08 -0700

    Fixed negative interval problem

--- timer1.js
+++ timer1.js
@@ -3,14 +3,16 @@ const intervals = process.argv.slice(2) //Don't need the first 2 elements
 let intervalsMS = [];
 
 for (const el of intervals) {
-  if (Number(el) >= 0) {
-    intervalsMS.push(el * 1000);
+  if (Number(el) && el >= 0) {
+    intervalsMS.push(el * 1000); 
   } 
 }
 
-for (let i = 0; i < intervals.length; i++) { //Loops how many times an element is added.
-  console.log(intervalsMS[i])
+for (let i = 0; i < intervalsMS.length; i++) { //Loops how many times an element is added.
+  console.log(`interval ${i + 1}: ${intervalsMS[i]}`)
   setTimeout( () => {
     process.stdout.write('\x07');
   }, intervalsMS[i])
 }
+
+//Note: For some reason, the intervals must be more than 1 second (1000ms) apart.
